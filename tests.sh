01a09e5cafc1bf3b60720b43e0df25927c209acf
Nyttep <duboispaul85170@hotmail.fr>
2024-11-20 17:05:42 +0100

    removed 'calculating discriminant' from tests

--- tests.sh
+++ tests.sh
@@ -50,38 +50,32 @@ The solution is:
 
 run_test "3 + 2 * X + 4 * X^2 = 0" "Reduced form : 3 + 2 * X + 4 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: -44 = 2^2 - 4 * 4 * 3
 Discriminant is strictly negative, there is no real solution"
 
 run_test "3 + 2 * X - 4 * X^2 = -8 + 7 * X ^2 - 5 * X + 9 * X^2" "Reduced form : 11 + 7 * X - 20 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: 929 = 7^2 - 4 * -20 * 11
 Discriminant is strictly positive, the two solutions are:
 -0.586988
 0.936988"
 
 run_test "0 = -8 + 7 * X ^2 - 5 * X + 9 * X^2" "Reduced form : 8 + 5 * X - 16 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: 537 = 5^2 - 4 * -16 * 8
 Discriminant is strictly positive, the two solutions are:
 -0.567914
 0.880414"
 
 run_test "4 * X^2 = 0" "Reduced form : 4 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: 0 = 0^2 - 4 * 4 * 0
 Discriminant is equal to zero, the solution is:
 0"
 
 run_test "0 = 4 * X^2" "Reduced form : -4 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: 0 = 0^2 - 4 * -4 * 0
 Discriminant is equal to zero, the solution is:
 0"
 
 run_test "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0" "Reduced form : 4 + 4 * X - 9.3 * X^2 = 0
 Polynomial degree: 2
-Calculating determinant: 164.8 = 4^2 - 4 * -9.3 * 4
 Discriminant is strictly positive, the two solutions are:
 -0.475131
 0.905239"
